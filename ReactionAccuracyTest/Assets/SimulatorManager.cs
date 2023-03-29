1bf729ba578891da6d714f6fa011ec9ba5ddbf9a
Sichun Wu <aliceSCW@163.com>
2023-03-29 12:34:39 +0200

    update

--- ReactionAccuracyTest/Assets/SimulatorManager.cs
+++ ReactionAccuracyTest/Assets/SimulatorManager.cs
@@ -11,18 +11,24 @@ public class SimulatorManager : MonoBehaviour
 {
     public GameObject target;
     public GameObject endPanel;
-    public float totalDuration = 5;
-    public float eachDuration = 1;
-    public float lastShowTime = 0;
-    public float lastHideTime = 0;
-    public float nextShowTime = 0;
-    public float maxDurationBetween = 3;
-    public int nrOfShows = 0;
-    public int maxNrOfShows = 10;
-    public float curTime = 0;
+    public GameObject breaktime;
+    public AudioClip[] musicClips;
+    //private float totalDuration = 15;
+    private float eachDuration = 1;
+    private float lastShowTime = 0;
+    private float lastHideTime = 0;
+    private float nextShowTime = 0;
+    private float maxDurationBetween = 3;
+    private int nrOfShows = 0;
+    private int maxNrOfShows = 10;
+    private float curTime = 0;
     Vector2 worldBoundary ;
-    public bool move = true;
-    public bool hit = false;
+    private bool move = true;
+    private bool hit = false;
+    private int roundNumber = 1;
+    private string componentName;
+    private int MaxRoundNumber = 6;
+    public string PID;
 
     public SimulatorManager _simulatorManager;
 
@@ -45,11 +51,27 @@ public class SimulatorManager : MonoBehaviour
 
     private List<KeyFrame> keyFrames = new List<KeyFrame>(10);
     // Start is called before the first frame update
+
+
     void Start()
     {
         target.SetActive(false);
         worldBoundary = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        ShuffleArray(musicClips);
+        GetComponent<AudioSource>().clip = musicClips[0];
+        GetComponent<AudioSource>().Play();
+        if (musicClips[0] != null)
+        {
+            componentName = musicClips[0].name;
+        }
+        else
+        {
+            componentName = "0";
+        }
+        Debug.Log(componentName);
     }
+
+
 
     void MoveTarget()
     {
@@ -70,7 +92,7 @@ public class SimulatorManager : MonoBehaviour
         }
         else
         {
-            if (curTime - lastHideTime > nextShowTime)
+            if (curTime - lastHideTime > nextShowTime && nrOfShows < maxNrOfShows)
             {
                 Vector2 camViewPoint = new Vector2(UnityEngine.Random.Range(-485, 493), UnityEngine.Random.Range(-306, 310));
                 target.transform.localPosition = camViewPoint;
@@ -78,29 +100,82 @@ public class SimulatorManager : MonoBehaviour
                 lastShowTime = curTime;
                 nrOfShows++;
             }
+            else if (nrOfShows == maxNrOfShows)
+            {
+                breaktime.SetActive(true);
+                GetComponent<AudioSource>().Stop();
+                Invoke("ResetNrOfShows", 15f);
+            }
         }
+    }
+    void ResetNrOfShows()
+    {
+
+        SaveToFile();
+        nrOfShows = 0;
+        breaktime.SetActive(false);
+        Debug.Log("roundNumber: " + (roundNumber));
+        try { 
+            GetComponent<AudioSource>().clip = musicClips[roundNumber];
+            GetComponent<AudioSource>().Play();
+            if (musicClips[roundNumber] != null)
+            {
+                componentName = musicClips[roundNumber].name;
+            }
+            else
+            {
+                componentName = "0";
+            }
+        }
+        catch {
+            componentName = "DontCount";
+        }
+
+
+        Debug.Log(componentName);
+        roundNumber++;
+        nrOfShows = 0;
+        lastHideTime = curTime;
+        nextShowTime = UnityEngine.Random.Range(0, maxDurationBetween);
+        CancelInvoke("ResetNrOfShows");
     }
 
     // Update is called once per frame
     void Update()
     {
         curTime += Time.deltaTime;
-        if (curTime > totalDuration || maxNrOfShows < nrOfShows)
-        {
-            if (move)
-            {
-                move = false;
-                target.SetActive(false);
-                endPanel.SetActive(true);
-                SaveToFile();
-            }
-        }
+
         if (move)
         {
             MoveTarget();
         }
-
+        if (roundNumber > MaxRoundNumber)
+        {
+            EndGame();
+        }
     }
+
+    private void Breaktime()
+    {
+        breaktime.SetActive(false);
+        CancelInvoke("Breaktime");
+        roundNumber++;
+        nrOfShows = 0;
+    }
+
+    public void EndGame()
+    {
+        if (move)
+        {
+            //MoveTarget();
+            GetComponent<AudioSource>().Stop();
+            move = false;
+            target.SetActive(false);
+            endPanel.SetActive(true);
+            SaveToFile();
+        }
+    }
+
 
     public void ClickOnTarget()
     {
@@ -132,6 +207,21 @@ public class SimulatorManager : MonoBehaviour
 
         return sb.ToString();
     }
+
+
+    void ShuffleArray(AudioClip[] array)
+    {
+        // Fisher-Yates shuffle algorithm
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int j = UnityEngine.Random.Range(0, i + 1);
+            AudioClip temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+
     public void SaveToFile()
     {
         // Use the CSV generation from before
@@ -142,11 +232,11 @@ public class SimulatorManager : MonoBehaviour
         var folder = Application.streamingAssetsPath;
 
         if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
-    #else
+#else
         var folder = Application.persistentDataPath;
-    #endif
+#endif
 
-        var filePath = Path.Combine(folder, "export.csv");
+        var filePath = Path.Combine(folder, PID, PID + "_" + componentName + ".csv");
 
         using (var writer = new StreamWriter(filePath, false))
         {
@@ -160,6 +250,7 @@ public class SimulatorManager : MonoBehaviour
 
     #if UNITY_EDITOR
         AssetDatabase.Refresh();
+        keyFrames = new List<KeyFrame>(10);
     #endif
     }
  }
